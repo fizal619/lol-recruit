bec1ef9af89b25adb1ce16aae6c4b7ec6a9d9585
Fizal Sarif <fizal.sarif@gmail.com>
2016-07-03 19:05:28 -0400

    messages successfully associated with users

--- public/js/script.js
+++ public/js/script.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
           return user.lol_id == $(stats[i]).attr('id')
         })
 
+        //populate the stats; TODO make these bars
         $(normal).text(user.stats[0].wins)
         $(ranked).text(user.stats[1].wins)
         $(assists).text(user.stats[0].aggregatedStats.totalAssists)
@@ -32,6 +33,24 @@ $(document).ready(function() {
   })
 
   // END POPULATE STATS
+
+  //POPULATE MESSAGES!
+  let messageContain = $('.contain')
+
+  //fetch the messages for the user, no need to pass arguments, it only returns messages for the logged in user.
+  $.get('/api/message/user', (messages)=>{
+    console.log(messages)
+    if(messages.length != 0){
+      $('#middle').remove()
+      let p = ''
+      messages.map((message)=>{
+        p += '<p class="message">'+ message.content + '<br><span id="sender">from: '+message.from+'</span></p>'
+      })
+      $(messageContain).html(p)
+    }
+  })
+
+  //END POPULATE MESSAGES
 
   // MODALLLLLL!!!!!!!!
 
@@ -42,24 +61,28 @@ $(document).ready(function() {
   })
 
   $('button.message').click((event)=>{
+    // get the ign of the button clicked
     let ign = event.target.dataset.ign
-
+    //function to send a message
     function sendMessage (){
       let data = {
         to: ign,
         from: $('#from').val(),
         content: $('#messageContent').val()
       }
+      //removes the click event listener to discourage spamming of the button
       $('#sendMessage').unbind()
-
+      //send a post to the url
       $.post('/api/message/new', data, (data)=> {
         console.log('sent')
+        //clean up and fase out
         $('#from').val('')
         $('#messageContent').val('')
         $('#modalOuter').fadeOut('fast')
       })
     }
 
+    //fade in the modal and set the click event on the send button
     $('#modalOuter').fadeIn('fast', (event)=>{
       $('#to').text(ign)
       $('#sendMessage').click(sendMessage)
